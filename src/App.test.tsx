3657e105813e7e745dcfde0a5e92625756ada810
nishant <nish54@gmail.com>
2023-09-25 17:35:05 -0500

    cleared sample app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('empty', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // expect().toBeInTheDocument();
 });
