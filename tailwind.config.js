da092742c3929f8f0c6dfc8c1d59c4bc0a4dc155
nishant <nish54@gmail.com>
2023-09-25 22:04:14 -0500

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  content: [
+    './src/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+  ],
   theme: {
     container: {
       center: true,
